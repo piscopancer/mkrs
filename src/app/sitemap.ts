9cc966664834f24ff53f0595caf2eedc8eb42274
piscopancer <igor.bistr01092003@gmail.com>
2024-01-25 22:12:39 +0300

    _

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute, Route } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return (['/', '/info', '/recent', '/saved', '/shortcuts'] satisfies Route[]).map((route) => ({
+  return (['/', '/info' as Route, '/recent', '/saved', '/shortcuts'] satisfies Route[]).map((route) => ({
     url: process.env.NEXT_PUBLIC_URL + route,
     lastModified: new Date(),
   }))
