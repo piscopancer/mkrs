6f8095aa8bcd762cb1e0f65cdcee3c72735ac0ff
piscopancer <igor.bistr01092003@gmail.com>
2024-01-27 14:21:27 +0300

    copyer disabled

--- src/app/(main)/search/[slug]/(private)/copyer.ts
+++ src/app/(main)/search/[slug]/(private)/copyer.ts
@@ -1,10 +1,6 @@
 'use client'
 
-import useShortcut from '@/hooks/use-key'
-import { searchStore } from '@/search'
-import { shortcuts } from '@/shortcuts'
-
 export default function Copyer({ search }: { search: string }) {
-  useShortcut([shortcuts.copy.keys, () => !searchStore.focused && navigator?.clipboard?.writeText(search)])
+  // useShortcut([shortcuts.copy.keys, () => !searchStore.focused && navigator?.clipboard?.writeText(search)])
   return null
 }
