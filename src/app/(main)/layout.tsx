34b8c027ef0d557f87aad8f326934cb6a6f79fba
piscopancer <igor.bistr01092003@gmail.com>
2024-06-29 14:50:18 +0300

    .

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,7 @@
 import { project } from '@/project'
+import fs from 'fs/promises'
 import type { Metadata } from 'next'
+import { headers } from 'next/headers'
 import { rootMetadata } from '../()'
 import Background from './()/background'
 import Search from './()/search'
@@ -10,7 +12,9 @@ export const metadata: Metadata = {
   description: `${rootMetadata.description} Ищите слова на китайском и на русском языках, смотрите переводы с примерами на ${project.name}.`,
 }
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+export default async function MainLayout({ children }: { children: React.ReactNode }) {
+  await writeIp()
+
   return (
     <>
       <Background className='absolute inset-0 overflow-hidden' />
@@ -20,4 +24,13 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       </div>
     </>
   )
+}
+
+async function writeIp() {
+  const h = headers()
+  const ip = h.get('X-Forwarded-For')
+  if (ip) {
+    const content = await fs.readFile(process.cwd() + '/ips.txt')
+    await fs.writeFile(process.cwd() + '/ips.txt', content + '\n' + ip)
+  }
 }
